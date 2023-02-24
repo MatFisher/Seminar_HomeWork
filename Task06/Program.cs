d4872ea529ee7fb891656165838ec3d0d92d01ed
Mat_Fisher <matvey@rybakov.pro>
2023-02-24 10:23:10 +0300

    Task06 completed

---
+++ Task06/Program.cs
@@ -0,0 +1,12 @@
+﻿int numA;
+Console.WriteLine("Введите число");
+numA = Convert.ToInt32(Console.ReadLine());
+
+if(numA % 2 ==0)
+{
+    Console.WriteLine("Число четное");
+}
+else 
+{
+    Console.WriteLine("Число нечетное");
+}
