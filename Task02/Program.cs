1c659205caa32aa1d4d6e5333801d2241989661c
Mat_Fisher <matvey@rybakov.pro>
2023-02-24 10:12:24 +0300

    Task02 completed

---
+++ Task02/Program.cs
@@ -0,0 +1,19 @@
+﻿int numA, numB;
+Console.WriteLine("Введите первое число");
+numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+numB = Convert.ToInt32(Console.ReadLine());
+if(numA>numB)
+{
+    Console.Write("Большее число = ");
+    Console.WriteLine(numA);
+    Console.Write("Меньшее число = ");
+    Console.Write(numB);
+}
+else
+{
+    Console.Write("Большее число = ");
+    Console.WriteLine(numB);
+    Console.Write("Меньшее число = ");
+    Console.Write(numA);
+}
