65be83e52b4291e575a448bc110d0bafdf4fb630
Mat_Fisher <matvey@rybakov.pro>
2023-02-24 10:19:55 +0300

    Task04 completed

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numA, numB, numC;
+Console.WriteLine("Введите первое число");
+numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+numC = Convert.ToInt32(Console.ReadLine());
+int max=numA;
+
+if(max < numB) max = numB;
+if(max < numC) max = numC;
+
+Console.Write("Максимальное число = ");
+Console.WriteLine(max);
