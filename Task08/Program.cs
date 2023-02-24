dbcc76fe1f5a38c3e472f20269ea56f58dc30854
Mat_Fisher <matvey@rybakov.pro>
2023-02-24 10:28:36 +0300

    Task08 completed

---
+++ Task08/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 0)
+{
+    int count = 2;
+    while(count<=number)
+    {
+        Console.Write (count + " ");
+        count = count+2;
+    
+    }
+
+}
+else 
+{
+    Console.WriteLine ("Введено не натуральное число");
+}
