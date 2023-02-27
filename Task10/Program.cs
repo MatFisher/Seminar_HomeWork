92245b6edb2223f87c3401683e82e2f08430d043
Mat_Fisher <matvey@rybakov.pro>
2023-02-27 16:27:15 +0300

    Программа,  которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа

---
+++ Task10/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SecOf3Digit(int num)
+{
+    int secDig = (num % 100)/10;
+    return secDig;
+}
+
+if (number >= 100 && number <= 999)
+{
+int secondDigit = SecOf3Digit(number);
+Console.Write($"Вторая цифра = {secondDigit}");
+}
+else Console.WriteLine("Введите корректное число");
