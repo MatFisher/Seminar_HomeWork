9da25431b1806c44c7d4821ab694be9248bcb135
Mat_Fisher <matvey@rybakov.pro>
2023-02-27 17:40:13 +0300

    Программа, которая выводит 3 цифру числа или сообщает, что ее нет

---
+++ Task13/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int FindThirdDigit(int num)
+{
+    int size = 0;
+    int index = 0;
+    int a = num;
+    while(a % 10 > 0) 
+    {
+        index = index + 1;
+        a = a / 10;
+    }
+    size = index - 3;
+    index = 0;
+    while(index < size)
+    {
+        num = num / 10;
+        index++;
+        }
+    num = num % 10;
+    return num;
+}
+
+if(number>100) 
+{
+    int thirdDigit = FindThirdDigit(number);
+    Console.WriteLine($"Третья цифра = {thirdDigit}");
+}
+else Console.WriteLine("Третья цифра отсутствует");
