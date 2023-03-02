f2bf73a153f76a355e6f18eea5683ce2c2cf0194
Mat_Fisher <matvey@rybakov.pro>
2023-03-02 15:27:17 +0300

    Проверить, является ли число палинромом

---
+++ Task19/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+
+
+string Palindrom(int num)
+{
+    if(num % 100000 / 10000 == num % 10 && num % 10000 / 1000 == num % 100 / 10) return "Палиндром";
+    else return "Не палиндром";
+}
+
+if(numberA.ToString().Length==5)
+{
+string result = Palindrom(numberA);
+Console.WriteLine(result);
+}
+else Console.WriteLine("Число не является пятизначным");
