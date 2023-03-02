e077a58d26e24e17d4b97d8b77e8abbe6ab55587
Mat_Fisher <matvey@rybakov.pro>
2023-03-02 15:35:17 +0300

    Таблица кубов чисел от 1 до N

---
+++ Task23/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Square(int num)
+{
+    int index = 1;
+    int result = 0;
+    while(index <= num)
+    {
+        result = index*index*index;
+        Console.WriteLine($"{index, 5}  {result, 5}");
+        index++;
+    }
+}
+
+Square(number);
