a5d2cd2969df5a0d2399eb3cf9b40c8babb14d2d
Mat_Fisher <matvey@rybakov.pro>
2023-03-06 15:42:56 +0300

    HomeWork 4

---
+++ Task25/Program.cs
@@ -0,0 +1,17 @@
+﻿int Exponent(int numA, int numB)
+{
+    int numHelp = numA;
+    for (int index = 1; index < numB; index++)
+    {
+        numA = numA * numHelp;
+    }
+    return numA;
+}
+
+Console.WriteLine("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int exp = Exponent(numberA, numberB);
+Console.WriteLine($"Результат = {exp}");
---
+++ Task27/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigitNumb(int num)
+{
+    int sum = 0;
+    while(num % 10 > 0) 
+    {
+        sum = sum + (num % 10);
+        num = num / 10;
+    }
+    return sum;
+}
+
+int summ = SumDigitNumb(number);
+Console.WriteLine($"Сумма цифр в числе = {summ}");
