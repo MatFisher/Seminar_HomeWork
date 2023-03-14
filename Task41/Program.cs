96cf29a4985f44682dc05bca25e6f355cf1a5030
Mat_Fisher <matvey@rybakov.pro>
2023-03-14 17:56:20 +0300

    Семинар 6

---
+++ Task41/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите планируемое количество чисел для ввода");
+int howMuch = Convert.ToInt32(Console.ReadLine());
+
+int[] EnterArray(int size)
+{
+    int[] array = new int [size];
+    for(int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Введите число");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int HowBigger0(int[] arr)
+{
+    int result = 0;
+    for(int i = 0; i < arr.Length; i++)
+    if(arr[i] > 0) result++;
+    return result;
+}
+
+int[] ar = EnterArray(howMuch);
+int count = HowBigger0(ar);
+Console.WriteLine($"Введено чисел больше 0 - {count}");
---
+++ Task43/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Введите четыре параметра функций");
+int howMuch = Convert.ToInt32(Console.ReadLine());
+
+int[] EnterArray(int size)
+{
+    int[] array = new int [size];
+    for(int i = 1; i < size+2; i++)
+    {
+        Console.WriteLine($"Параметр {i}");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+double[] Coord(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[0] == arr[2]) Console.WriteLine("Прямые параллельны");
+        if(arr[i] == arr[i+1] && arr[i+1] == arr[i+2]) Console.WriteLine("Прямые совпадают");
+    }
+    double[] ar = new double[arr.Length/2];
+    ar[0] = (arr[3] - arr[1]) / (arr[0] - arr[2]);
+    ar[1] = arr[0] * ar[0] + arr[1];
+    return ar;
+} 
+
+void PrintArrayDouble (double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]} | ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] m = EnterArray(howMuch);
+double[] xy = Coord(m);
+PrintArrayDouble(xy);
+
+
+
